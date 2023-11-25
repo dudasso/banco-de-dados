d95689b2860ba4d03bbd22feb3f2f1e6dda4bf1d
guatxinim <imaciel2917@gmail.com>
2023-11-25 15:47:40 -0300

    adicionou o template base e o da página inicial

--- delivery/views.py
+++ delivery/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from delivery.models import *
+
 # Create your views here.
 def lista_restaurantes(request):
-    return render(request, 'delivery/lista_restaurantes.html', {})
+    restaurantes = Restaurante.objects.all()
+    return render(request, 'delivery/lista_restaurantes.html', {'restaurantes': restaurantes})
